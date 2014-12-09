8943274ecb0697520c795146f20717433d0dc6cd
laosi <1124519074@qq.com>
2014-12-09 16:40:16 +0800

    update print.c

--- fake_template/print.h
+++ fake_template/print.h
@@ -1,7 +1,7 @@
 #ifndef PRINT_H
 #define PRINT_H
-#define printA(x,n) _Generic((x), int *:print_intArray,	\
-				  double *:print_doubleArray)(x,n)
+#define printA(x,n) _Generic((x), int    *: print_intArray,	\
+				  double *: print_doubleArray)(x, n)
 
 void print_intArray(int *array, int n);
 
